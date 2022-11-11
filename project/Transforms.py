89d33cc17ac4a874dff4259c4396d9afe64c7981
Chris Benka <chrisbenka@gmail.com>
2022-11-11 12:43:14 -0500

    Add transforms

---
+++ project/Transforms.py
@@ -0,0 +1,5 @@
+class ThresholdTransform(object):
+  def __init__(self, thr_255):
+    self.thr = thr_255 / 255.  # input threshold for [0..255] gray level, convert to [0..1]
+  def __call__(self, x):
+    return (x > self.thr).to(x.dtype)
